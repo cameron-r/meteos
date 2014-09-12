a894336ba76a60ab08985a0f409531718646cbfc
Cameron <crowshanbin@gmail.com>
2014-09-12 03:39:18 -0500

    Started project

---
+++ core/src/com/meteos/game/MeteosGame.java
@@ -0,0 +1,27 @@
+package com.meteos.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MeteosGame extends Game {
+
+    public SpriteBatch batch;
+    public BitmapFont font;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        this.setScreen(new MainMenuScreen(this));
+    }
+
+    public void render() {
+        super.render();
+    }
+
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
+}
